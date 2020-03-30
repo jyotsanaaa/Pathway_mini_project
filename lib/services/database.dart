8540875766dadcbf130b47cdd1a029c557ac87ad
Aadit Babu Pant <pantaadit@gmail.com>
2020-03-30 13:25:23 +0545

    working on creating database

---
+++ lib/services/database.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  // String currentLocation, destination;
+  // String uid;
+
+  // DatabaseService({this.currentLocation, this.destination});
+  // DatabaseService({this.uid});
+
+  // collection refrence
+  final CollectionReference inpDB = Firestore.instance.collection('userInputs');
+
+  updateUserData(String currentLocation, String destination){
+    return inpDB.document().setData({
+      'CurrentLocation': currentLocation,
+      'Destination': destination,
+    });
+  }
+
+}
